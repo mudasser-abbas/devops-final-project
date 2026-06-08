fb0cb2e1487a278561fd0f6ff3daed165e117e0a
Mudasser Abbas <mudasser.abbas@gmail.com>
2026-06-08 16:54:40 +0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const os = require('os');
+
+const app = express();
+const PORT = 3000;
+
+let visitors = 0;
+
+app.get('/', (req, res) => {
+
+    visitors++;
+
+    res.send(`
+    <html>
+    <head>
+      <title>DevOps Final Project</title>
+    </head>
+    <body>
+      <h1>DevOps Final Project</h1>
+
+      <h2>Muhammad Mudasser Abbas</h2>
+
+      <p><b>Current Time:</b> ${new Date()}</p>
+
+      <p><b>Container Host:</b> ${os.hostname()}</p>
+
+      <p><b>Visitors:</b> ${visitors}</p>
+
+    </body>
+    </html>
+    `);
+});
+
+app.get('/health', (req,res)=>{
+    res.send("Application Healthy");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});

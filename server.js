8f946cbb4d658a79dea297249a5a6c528548515b
Mudasser Abbas <mudasser.abbas@gmail.com>
2026-06-09 12:17:44 +0500

    Update server page

--- server.js
+++ server.js
@@ -2,43 +2,44 @@ const express = require('express');
 const os = require('os');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 let visitors = 0;
 
+// Home Page
 app.get('/', (req, res) => {
-
     visitors++;
 
     res.send(`
-   
 <html>
 <head>
-<title>DevOps Final Project</title>
+    <title>DevOps Final Project</title>
 </head>
 <body>
 
-<h1>DevOps Final Project - CI/CD Test</h1>
+    <h1>DevOps Final Project - CI/CD Test</h1>
 
-<h2>Muhammad Mudasser Abbas</h2>
+    <h2>Muhammad Mudasser Abbas</h2>
 
-<p><b>Version:</b> V2 deployed through GitHub Actions</p>
+    <p><b>Version:</b> V2 deployed through GitHub Actions</p>
 
-<p><b>Current Time:</b> ${new Date()}</p>
+    <p><b>Current Time:</b> ${new Date()}</p>
 
-<p><b>Container Host:</b> ${os.hostname()}</p>
+    <p><b>Container Host:</b> ${os.hostname()}</p>
 
-<p><b>Visitors:</b> ${visitors}</p>
+    <p><b>Visitors:</b> ${visitors}</p>
 
 </body>
 </html>
-` );
-
-app.get('/health', (req,res)=>{
-    res.send("Application Healthy");
+    `);
 });
 
+// Health Check Endpoint
+app.get('/health', (req, res) => {
+    res.send('Application Healthy');
+});
+
+// Start Server
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
-
